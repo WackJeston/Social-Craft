3eafb9687d85cca0af27c57ac5891674dbb85c34
WackJeston <33kcaj33@gmail.com>
2022-03-14 14:05:29 +0000

    Commit

---
+++ web/js/site-menu.js
@@ -0,0 +1,27 @@
+const body = document.body;
+const menuButton = document.querySelector("#header-menu-button")
+const page = document.querySelector(".page-container")
+const pageCover = document.querySelector(".page-overlay")
+let activeMenu = false
+
+
+menuButton.addEventListener("click", function(){
+  if (activeMenu == false){
+    body.classList.remove("menu-non-active");
+    body.classList.add("menu-active");
+    pageCover.style.display = "flex";
+    activeMenu = true;
+  } else if (activeMenu == true){
+    body.classList.remove("menu-active");
+    body.classList.add("menu-non-active");
+    pageCover.style.display = "none";
+    activeMenu = false;
+  }
+})
+
+pageCover.addEventListener("click", function(){
+  body.classList.remove("menu-active");
+  body.classList.add("menu-non-active");
+  pageCover.style.display = "none";
+  activeMenu = false;
+})
